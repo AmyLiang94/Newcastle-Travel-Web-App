0cc0dcfca2415f86ecdb50d21af8ddb21615ad71
m0_60776279 <571379772@qq.com>
2023-04-20 11:30:34 +0100

    merge diffenerce in attrService

--- app-main/src/main/java/com/groupwork/charchar/service/impl/AttractionsServiceImpl.java
+++ app-main/src/main/java/com/groupwork/charchar/service/impl/AttractionsServiceImpl.java
@@ -1,26 +1,37 @@
 package com.groupwork.charchar.service.impl;
 
-import com.google.gson.*;
-import com.google.maps.model.OpeningHours;
-import com.google.maps.model.PlacesSearchResponse;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.groupwork.charchar.common.Constants;
+import com.groupwork.charchar.dao.AttractionsDao;
+import com.groupwork.charchar.entity.AttractionsEntity;
+import com.groupwork.charchar.entity.ReviewsEntity;
+import com.groupwork.charchar.exception.AttractionNotFoundException;
+import com.groupwork.charchar.service.AttractionsService;
+import com.groupwork.charchar.service.ReviewsService;
 import com.groupwork.charchar.vo.UpdateAttractionRatingVO;
 import lombok.SneakyThrows;
-import okhttp3.*;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import sun.net.www.protocol.http.HttpURLConnection;
 
+import javax.annotation.Resource;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.URI;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -28,52 +39,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-
-import com.groupwork.charchar.dao.AttractionsDao;
-import com.groupwork.charchar.entity.AttractionsEntity;
-import com.groupwork.charchar.service.AttractionsService;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-import sun.net.www.protocol.http.HttpURLConnection;
-
-import java.net.http.HttpClient;
-
-import javax.xml.transform.Result;
-
 
 @Service("attractionsService")
 public class AttractionsServiceImpl extends ServiceImpl<AttractionsDao, AttractionsEntity> implements AttractionsService {
+    private Logger logger = LoggerFactory.getLogger(AttractionsServiceImpl.class);
     @Value("${google.maps.api.key}")
     private String key;
-    @Autowired
-    private RestTemplate restTemplate;
+    @Resource
+    private AttractionsDao attractionsDao;
+    @Resource
+    private ReviewsService reviewsService;
 
     @Override
     public List<AttractionsEntity> getNearByLocation(double latitude, double longitude, double radius) throws IOException {
         List<AttractionsEntity> showList = new ArrayList<>();
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
-        MediaType mediaType = MediaType.parse("text/plain");
-        //RequestBody body = RequestBody.create(mediaType, "");
         String url = String.format("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%f,%f&radius=%f&type=tourist_attraction&key=%s", latitude, longitude, radius, key);
         Request request = new Request.Builder()
                 .url(url)
                 .build();
         Response response = client.newCall(request).execute();
-        // 解析响应数据
-        //Gson gson = new Gson();
         JsonObject json = JsonParser.parseString(response.body().string()).getAsJsonObject();
         //获取列表
         JsonArray datas = json.getAsJsonArray("results");
         for (JsonElement data : datas) {
             JsonObject curPlace = data.getAsJsonObject();
             String name = curPlace.get("name").getAsString();
-//            String icon = curPlace.get("icon").getAsString();
             double lat = curPlace.getAsJsonObject("geometry").getAsJsonObject("location").get("lat").getAsDouble();
             double lng = curPlace.getAsJsonObject("geometry").getAsJsonObject("location").get("lng").getAsDouble();
             String photo = curPlace.getAsJsonArray("photos").get(0).getAsJsonObject().get("photo_reference").getAsString();
-            // json to AttractionEntity
             AttractionsEntity attractions = new AttractionsEntity();
             attractions.setAttractionName(name);
             attractions.setLatitude(BigDecimal.valueOf(lat));
@@ -91,19 +86,25 @@ public class AttractionsServiceImpl extends ServiceImpl<AttractionsDao, Attracti
 
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
-        MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, "");
-        String url = String.format("https://maps.googleapis.com/maps/api/directions/json?origin%f,%f&destination=%f,%f&key=%s", departLat, departLng, desLat, desLng, key);
+        String url = String.format("https://maps.googleapis.com/maps/api/directions/json?origin=%.6f,%.6f&destination=%.6f,%.6f&mode=walking&key=%s", departLat, departLng, desLat, desLng, key);
         Request request = new Request.Builder()
                 .url(url)
-                .method("GET", body)
                 .build();
-            Response response = client.newCall(request).execute();
+        Response response = client.newCall(request).execute();
         JsonObject json = JsonParser.parseString(response.body().string()).getAsJsonObject();
-        JsonObject walk = json.getAsJsonArray("routes").get(0).getAsJsonObject().getAsJsonArray("leg").get(0).getAsJsonObject();
-        String time = walk.getAsJsonObject("duration").get("text").getAsString();
-        return time;
-
+        System.out.println(json.toString());
+        JsonArray routes = json.getAsJsonArray("routes");
+        System.out.println(routes.toString());
+        if (routes != null && routes.size() > 0) {
+            JsonObject firstRoute = routes.get(0).getAsJsonObject();
+            JsonArray legs = firstRoute.getAsJsonArray("legs");
+            if (legs != null && legs.size() > 0) {
+                JsonObject walk = legs.get(0).getAsJsonObject();
+                String time = walk.getAsJsonObject("duration").get("text").getAsString();
+                return time;
+            }
+        }
+        return "can't access";
     }
 
     @Override
@@ -112,8 +113,6 @@ public class AttractionsServiceImpl extends ServiceImpl<AttractionsDao, Attracti
         List<AttractionsEntity> filteredAttractions = attractions.stream().filter(a -> a
                         .getCategory().equals(category))
                 .collect(Collectors.toList());
-
-
 
         return filteredAttractions;
     }
@@ -126,10 +125,6 @@ public class AttractionsServiceImpl extends ServiceImpl<AttractionsDao, Attracti
                 filteredAttractions.add(attraction);
             }
         }
-
-
-
-
         return filteredAttractions;
     }
     @SneakyThrows
@@ -206,7 +201,37 @@ public class AttractionsServiceImpl extends ServiceImpl<AttractionsDao, Attracti
 
     @Override
     public UpdateAttractionRatingVO updateAttractionRating(Integer attractionId) {
-        return null;
+        AttractionsEntity attraction = attractionsDao.getAttractionById(attractionId);
+        if (attraction == null) {
+            throw new AttractionNotFoundException(Constants.ResponseCode.UN_ERROR.getCode(), Constants.ResponseCode.UN_ERROR.getInfo() + ": " + attractionId);
+        }
+        Double attrSumRating = 0d;
+        Integer reviewCount = 0;
+        List<ReviewsEntity> reviews = reviewsService.listReviewsByAttractionId(attractionId);
+        // if there is no one review the attraction, if will return a empty attraction entity.
+        if (reviews == null || reviews.size() == 0) {
+            return new UpdateAttractionRatingVO();
+        }
+        for (ReviewsEntity review : reviews) {
+            attrSumRating += review.getRating();
+            reviewCount++;
+        }
+        Double attrRating = attrSumRating / reviewCount;
+        DecimalFormat decimalFormat = new DecimalFormat("#.#");
+        attrRating = Double.parseDouble(decimalFormat.format(attrRating));
+        try {
+            int updateStatus = attractionsDao.updateAttractionRating(attractionId, attrRating);
+            if (updateStatus == 0) {
+                throw new RuntimeException("update fail , attraction : " + attractionId);
+            }
+        } catch (Exception e) {
+            logger.error("Failed to update attraction rating, attraction ID: " + attractionId, e);
+            throw new RuntimeException("can't update, attraction : " + attractionId);
+        }
+        UpdateAttractionRatingVO updateAttraction = new UpdateAttractionRatingVO();
+        updateAttraction.setAttractionId(attractionId);
+        updateAttraction.setAttrRating(attrRating);
+        return updateAttraction;
     }
 
 
